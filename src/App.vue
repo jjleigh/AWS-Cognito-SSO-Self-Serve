59911eed14475567f15ced385992522d3c899e56
Justine <justinejones91@gmail.com>
2023-02-11 09:16:29 -0800

    moving nav to the home file

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <Home />
+  <v-app>
+    <v-main>
+      <v-app-bar color="deep-purple accent-4" dense dark>
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </v-app-bar>
+      <v-navigation-drawer
+        v-model="drawer"
+        temporary
+      >
+        <v-list :items="items"></v-list>
+      </v-navigation-drawer>
+      <Home />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -9,6 +22,17 @@ export default {
   name: 'App',
   components: {
     Home
+  },
+  data: () => ({
+    drawer: false,
+    items: [
+      { title: 'Settings' }
+    ],
+  }),
+  watch: {
+    group () {
+      this.drawer = false
+    },
   }
 }
 </script>
@@ -21,5 +45,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,50 +1,15 @@
 <template>
-  <v-app>
-    <v-main>
-        <v-app-bar color="deep-purple accent-4" dense dark>
-          <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      </v-app-bar>
-      <v-navigation-drawer
-        v-model="drawer"
-        temporary
-      >
-        <v-list :items="items"></v-list>
-      </v-navigation-drawer>
-    </v-main>
-  </v-app>
+  <v-main>
+    <p>This is Home</p>
+  </v-main>
 </template>
 
 <script>
 export default {
   name: 'HomeScreen',
-  data: () => ({
-    drawer: false,
-    items: [
-      { title: 'Settings' }
-    ],
-  }),
-  watch: {
-    group () {
-      this.drawer = false
-    },
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
