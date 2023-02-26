fe5d0266b420ca30748f1cb5c424d246559dd7f5
Justine <justinejones91@gmail.com>
2023-02-26 11:55:27 -0800

    vue 3 unit test setup

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
+// module.exports = {
+//   presets: [
+//     '@vue/cli-plugin-babel/preset'
+//   ]
+// }
+
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
