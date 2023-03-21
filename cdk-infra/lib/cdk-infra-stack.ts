ee2c6a591f2edbfb632e56712e20975e7c23c031
Justine <justinejones91@gmail.com>
2023-03-21 14:53:59 -0700

    adding cloudfront distribution to CDK

--- cdk-infra/lib/cdk-infra-stack.ts
+++ cdk-infra/lib/cdk-infra-stack.ts
@@ -1,6 +1,8 @@
 import { App, Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import {
   aws_s3 as s3,
+  aws_cloudfront as cloudfront,
+  aws_ssm as ssm,
 } from 'aws-cdk-lib';
 
 interface MyStackProps extends StackProps {
@@ -9,14 +11,17 @@ interface MyStackProps extends StackProps {
     prefix: string; // prefix for all resources
   };
 }
-
 export class CDKInfraStack extends Stack {
   constructor(scope: App, id: string, props?: MyStackProps) {
     super(scope, id, props);
 
     const { stage, prefix } = props?.context || {};
 
-    const bucket = new s3.Bucket(this, 'MyBucket', {
+    /**************************************************************
+     * S3 DEPLOYMENT BUCKET 
+    **************************************************************/
+
+    const uiBucket = new s3.Bucket(this, 'UIBucket', {
       bucketName: `${prefix}-ui-${stage}`,
       accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
@@ -24,6 +29,51 @@ export class CDKInfraStack extends Stack {
       websiteIndexDocument: 'index.html',
     });
 
-    new CfnOutput(this, 'exampleBucketArn', { value: bucket.bucketArn });
+    const originAccessIdentity = new cloudfront.OriginAccessIdentity(this, "uiDeploymentBucketOAI", {
+      comment: "UI-DeploymentBucket-OAI"
+    });
+    uiBucket.grantRead(originAccessIdentity);
+
+
+    /**************************************************************
+     * CLOUDFRONT DISTRIBUTION 
+    **************************************************************/
+    const cloudFrontWebDistribution = new cloudfront.CloudFrontWebDistribution(this, "UIDistribution", {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            // id: `${props.envObject.Prefix}-build`,
+            s3BucketSource: uiBucket,
+            originAccessIdentity: originAccessIdentity,
+            originPath: "/build" },
+          behaviors: [
+            { isDefaultBehavior: true }
+          ],
+        }
+      ],
+      comment: "UI Distribution"
+    });
+    
+    //set a parameter for the cloudfront url
+    new ssm.StringParameter(this, 'UICloudFrontURL', {
+      description: 'UI CloudFront url',
+      parameterName: `/${prefix}/cloudfront/distributionUrl`,
+      stringValue: `https://${cloudFrontWebDistribution.distributionDomainName}`
+    });
+
+    //set a parameter for the cloudfront destribution id
+    new ssm.StringParameter(this, 'UICloudFrontDistributionId', {
+      description: 'UI CloudFront Distribution ID',
+      parameterName: `/${prefix}/cloudfront/distributionId`,
+      stringValue: cloudFrontWebDistribution.distributionId
+    });
+
+    /**************************************************************
+     * OUTPUTS 
+    **************************************************************/
+
+    new CfnOutput(this, 'BucketArn', { value: uiBucket.bucketArn });
+    new CfnOutput(this, "cloudFrontUrl", { value: `https://${cloudFrontWebDistribution.distributionDomainName}`});
+    new CfnOutput(this, "cloudFrontDistributionId", { value: cloudFrontWebDistribution.distributionId});
   }
 }
